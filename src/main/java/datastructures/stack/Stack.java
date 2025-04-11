9260721ede103a43338271515c104735b5b09081
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-04-11 08:02:52 -0300

    Stack constructor

---
+++ src/main/java/datastructures/stack/Stack.java
@@ -0,0 +1,37 @@
+package datastructures.stack;
+
+public class Stack {
+    Node top;
+    int height;
+
+    class Node {
+        Node next;
+        int value;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getTop(){
+        System.out.println("Top: " + top.value);
+    }
+
+    public void getHeight(){
+        System.out.println("Height: " + height);
+    }
+}
