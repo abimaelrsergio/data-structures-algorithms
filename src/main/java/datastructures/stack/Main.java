b122dea448671f091c5137de51e66b1953b25ee9
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-04-11 08:17:46 -0300

    stack pop

--- src/main/java/datastructures/stack/Main.java
+++ src/main/java/datastructures/stack/Main.java
@@ -2,12 +2,14 @@ package datastructures.stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack myStack = new Stack(2);
-
+        Stack myStack = new Stack(7);
         //myStack.getTop();
         //myStack.getHeight();
+        myStack.push(23);
+        myStack.push(3);
+        myStack.push(11);
 
-        myStack.push(1);
+        myStack.pop();
 
         myStack.printStack();
     }
