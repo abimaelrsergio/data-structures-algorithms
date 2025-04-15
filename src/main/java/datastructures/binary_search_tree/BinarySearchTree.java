44bff4d5f1138a183afb89c018a04ff7b62ccf22
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-04-15 07:40:53 -0300

    Insert new Node in the Binary Search Tree

--- src/main/java/datastructures/binary_search_tree/BinarySearchTree.java
+++ src/main/java/datastructures/binary_search_tree/BinarySearchTree.java
@@ -5,14 +5,38 @@ public class BinarySearchTree {
     Node root;
 
     class Node {
-        public int value;
-        public Node left;
-        public Node right;
+        int value;
+        Node left;
+        Node right;
 
-        public Node(int value) {
+        Node(int value) {
             this.value = value;
         }
     }
 
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while(true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
 
 }
--- src/main/java/temp/Solution.java
+++ src/main/java/temp/Solution.java
@@ -1,6 +1,12 @@
 package temp;
 
 public class Solution {
+    /**
+     * Tests the solution method with an array {1,6} and target 7,
+     * and prints the result.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         int[] array = {1,6};
         int target = 7;
@@ -12,6 +18,14 @@ public class Solution {
             System.out.println(solution);
     }
 
+    /**
+     * Finds two indices in the array such that the numbers at those indices add up to the target.
+     *
+     * @param array the input array of integers
+     * @param target the target sum to find in the array
+     * @return an array of two integers representing the indices of the two numbers
+     *         that add up to the target, or null if no such pair exists
+     */
     public static int[] solution(int[] array, int target) {
         if (array == null || array.length == 0 || array.length == 1) return null;
         for (int index = 0; index < array.length; index++) {
