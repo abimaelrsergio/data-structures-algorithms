3e0bec5f382462ee1f393df8fe079761cee6d9f4
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-04-24 07:58:29 -0300

    The get method added

--- src/main/java/datastructures/hashtable/HashTable.java
+++ src/main/java/datastructures/hashtable/HashTable.java
@@ -33,6 +33,16 @@ public class HashTable {
         }
     }
 
+    public int get(String key) {
+        int index = hash(key);
+        Node temp = dataMap[index];
+        while(temp != null) {
+            if (temp.key.equals(key)) return temp.value;
+            temp = temp.next;
+        }
+        return 0;
+    }
+
     private int hash(String key) {
         int hash = 0;
         char[] keyChars = key.toCharArray();
