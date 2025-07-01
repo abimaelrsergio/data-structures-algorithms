403e57e27d8b8bc7ecf119a749849bab2416701b
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-07-01 08:29:44 -0300

    Add the Linked List and the printList() method

---
+++ src/main/java/datastructures/linked_list/Main.java
@@ -0,0 +1,12 @@
+package datastructures.linked_list;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.printList();
+    }
+}
