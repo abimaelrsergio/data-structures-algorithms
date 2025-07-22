dd33a26262514623193857070443b4d13555c460
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-07-22 08:42:47 -0300

    linked list update

--- src/main/java/datastructures/linked_list/Main.java
+++ src/main/java/datastructures/linked_list/Main.java
@@ -3,11 +3,16 @@ package datastructures.linked_list;
 public class Main {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.add(5);
+
+
 
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
         myLinkedList.printList();
+        System.out.println("--------------------");
+        LinkedList.Node node = myLinkedList.removeLast();
+        myLinkedList.printList();
+        System.out.println("Node = " + node.value);
     }
 }
