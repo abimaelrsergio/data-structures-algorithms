f1516bae0f15dd8b55ccedd98718d2e9ddecd4b5
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-07-24 07:31:55 -0300

    Including the get method

--- src/main/java/datastructures/linked_list/Main.java
+++ src/main/java/datastructures/linked_list/Main.java
@@ -2,11 +2,13 @@ package datastructures.linked_list;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(2);
+        LinkedList myLinkedList = new LinkedList(0);
         myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
 
-        System.out.println(myLinkedList.removeFirst().value);
-        System.out.println(myLinkedList.removeFirst().value);
-        System.out.println(myLinkedList.removeFirst());
+        System.out.println(myLinkedList.get(2).value + "\n");
+
+        myLinkedList.printList();
     }
 }
